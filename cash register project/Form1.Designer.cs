692643a5bfde89d197370dccbbdc8b809c994066
P1 <P1@DESKTOP-QCIIOTF>
2021-09-30 13:54:48 -0400

    Add project files.

---
+++ cash register project/Form1.Designer.cs
@@ -0,0 +1,245 @@
+﻿
+namespace cash_register_project
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.titleLabel = new System.Windows.Forms.Label();
+            this.spiderlegButton = new System.Windows.Forms.Label();
+            this.snailslimeButton = new System.Windows.Forms.Label();
+            this.ratwhiskersButton = new System.Windows.Forms.Label();
+            this.legsInput = new System.Windows.Forms.TextBox();
+            this.whiskersInput = new System.Windows.Forms.TextBox();
+            this.slimeInput = new System.Windows.Forms.TextBox();
+            this.totalButton = new System.Windows.Forms.Button();
+            this.totallabel = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.taxlabel = new System.Windows.Forms.Label();
+            this.sublabel = new System.Windows.Forms.Label();
+            this.outputlabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // titleLabel
+            // 
+            this.titleLabel.AutoSize = true;
+            this.titleLabel.Location = new System.Drawing.Point(50, 19);
+            this.titleLabel.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.titleLabel.Name = "titleLabel";
+            this.titleLabel.Size = new System.Drawing.Size(194, 28);
+            this.titleLabel.TabIndex = 0;
+            this.titleLabel.Text = "The Potion Shop";
+            // 
+            // spiderlegButton
+            // 
+            this.spiderlegButton.AutoSize = true;
+            this.spiderlegButton.Font = new System.Drawing.Font("MV Boli", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.spiderlegButton.Location = new System.Drawing.Point(12, 84);
+            this.spiderlegButton.Name = "spiderlegButton";
+            this.spiderlegButton.Size = new System.Drawing.Size(93, 17);
+            this.spiderlegButton.TabIndex = 1;
+            this.spiderlegButton.Text = "Spider Legs:";
+            // 
+            // snailslimeButton
+            // 
+            this.snailslimeButton.AutoSize = true;
+            this.snailslimeButton.Font = new System.Drawing.Font("MV Boli", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.snailslimeButton.Location = new System.Drawing.Point(12, 116);
+            this.snailslimeButton.Name = "snailslimeButton";
+            this.snailslimeButton.Size = new System.Drawing.Size(89, 17);
+            this.snailslimeButton.TabIndex = 2;
+            this.snailslimeButton.Text = "Snail Slime:";
+            // 
+            // ratwhiskersButton
+            // 
+            this.ratwhiskersButton.AutoSize = true;
+            this.ratwhiskersButton.Font = new System.Drawing.Font("MV Boli", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ratwhiskersButton.Location = new System.Drawing.Point(12, 145);
+            this.ratwhiskersButton.Name = "ratwhiskersButton";
+            this.ratwhiskersButton.Size = new System.Drawing.Size(104, 17);
+            this.ratwhiskersButton.TabIndex = 3;
+            this.ratwhiskersButton.Text = "Rat Whiskers:";
+            // 
+            // legsInput
+            // 
+            this.legsInput.Font = new System.Drawing.Font("MV Boli", 6.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.legsInput.Location = new System.Drawing.Point(122, 86);
+            this.legsInput.Name = "legsInput";
+            this.legsInput.Size = new System.Drawing.Size(153, 22);
+            this.legsInput.TabIndex = 4;
+            // 
+            // whiskersInput
+            // 
+            this.whiskersInput.Font = new System.Drawing.Font("MV Boli", 6.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.whiskersInput.Location = new System.Drawing.Point(122, 142);
+            this.whiskersInput.Name = "whiskersInput";
+            this.whiskersInput.Size = new System.Drawing.Size(153, 22);
+            this.whiskersInput.TabIndex = 5;
+            // 
+            // slimeInput
+            // 
+            this.slimeInput.Font = new System.Drawing.Font("MV Boli", 6.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.slimeInput.Location = new System.Drawing.Point(122, 114);
+            this.slimeInput.Name = "slimeInput";
+            this.slimeInput.Size = new System.Drawing.Size(153, 22);
+            this.slimeInput.TabIndex = 6;
+            // 
+            // totalButton
+            // 
+            this.totalButton.ForeColor = System.Drawing.Color.Black;
+            this.totalButton.Location = new System.Drawing.Point(67, 179);
+            this.totalButton.Name = "totalButton";
+            this.totalButton.Size = new System.Drawing.Size(161, 42);
+            this.totalButton.TabIndex = 7;
+            this.totalButton.Text = "Total";
+            this.totalButton.UseVisualStyleBackColor = true;
+            this.totalButton.Click += new System.EventHandler(this.totalButton_Click);
+            // 
+            // totallabel
+            // 
+            this.totallabel.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.totallabel.Font = new System.Drawing.Font("MV Boli", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.totallabel.ForeColor = System.Drawing.Color.Black;
+            this.totallabel.Location = new System.Drawing.Point(122, 329);
+            this.totallabel.Name = "totallabel";
+            this.totallabel.Size = new System.Drawing.Size(154, 22);
+            this.totallabel.TabIndex = 8;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("MV Boli", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(16, 329);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 17);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Total:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("MV Boli", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 290);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(98, 17);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Tax Amount:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("MV Boli", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(16, 249);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(74, 17);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Subtotal:";
+            // 
+            // taxlabel
+            // 
+            this.taxlabel.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.taxlabel.Font = new System.Drawing.Font("MV Boli", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.taxlabel.ForeColor = System.Drawing.Color.Black;
+            this.taxlabel.Location = new System.Drawing.Point(122, 288);
+            this.taxlabel.Name = "taxlabel";
+            this.taxlabel.Size = new System.Drawing.Size(154, 22);
+            this.taxlabel.TabIndex = 12;
+            // 
+            // sublabel
+            // 
+            this.sublabel.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.sublabel.Font = new System.Drawing.Font("MV Boli", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.sublabel.ForeColor = System.Drawing.Color.Black;
+            this.sublabel.Location = new System.Drawing.Point(122, 247);
+            this.sublabel.Name = "sublabel";
+            this.sublabel.Size = new System.Drawing.Size(154, 22);
+            this.sublabel.TabIndex = 13;
+            // 
+            // outputlabel
+            // 
+            this.outputlabel.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.outputlabel.Font = new System.Drawing.Font("MV Boli", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.outputlabel.ForeColor = System.Drawing.Color.Black;
+            this.outputlabel.Location = new System.Drawing.Point(471, 19);
+            this.outputlabel.Name = "outputlabel";
+            this.outputlabel.Size = new System.Drawing.Size(239, 322);
+            this.outputlabel.TabIndex = 14;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 28F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(731, 369);
+            this.Controls.Add(this.outputlabel);
+            this.Controls.Add(this.sublabel);
+            this.Controls.Add(this.taxlabel);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.totallabel);
+            this.Controls.Add(this.totalButton);
+            this.Controls.Add(this.slimeInput);
+            this.Controls.Add(this.whiskersInput);
+            this.Controls.Add(this.legsInput);
+            this.Controls.Add(this.ratwhiskersButton);
+            this.Controls.Add(this.snailslimeButton);
+            this.Controls.Add(this.spiderlegButton);
+            this.Controls.Add(this.titleLabel);
+            this.Font = new System.Drawing.Font("MV Boli", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.Color.OrangeRed;
+            this.Margin = new System.Windows.Forms.Padding(6);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label titleLabel;
+        private System.Windows.Forms.Label spiderlegButton;
+        private System.Windows.Forms.Label snailslimeButton;
+        private System.Windows.Forms.Label ratwhiskersButton;
+        private System.Windows.Forms.TextBox legsInput;
+        private System.Windows.Forms.TextBox whiskersInput;
+        private System.Windows.Forms.TextBox slimeInput;
+        private System.Windows.Forms.Button totalButton;
+        private System.Windows.Forms.Label totallabel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label taxlabel;
+        private System.Windows.Forms.Label sublabel;
+        private System.Windows.Forms.Label outputlabel;
+    }
+}
+
---
+++ cash register project/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace cash_register_project
+{
+    public partial class Form1 : Form
+    {
+
+        double spiderlegs = 8.75;
+        double snailslime = 5.00;
+        double ratwhiskers = 2.30;
+
+        int numOflegs = 0;
+        int numOfslime = 0;
+        int numOfwhiskers = 0;
+
+        double subtotal = 0;
+        double totalprice = 0;
+
+        double taxrate = 0.13;
+        double taxamount = 0;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void totalButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                numOflegs = Convert.ToInt32(legsInput.Text);
+                numOfslime = Convert.ToInt32(slimeInput.Text);
+                numOfwhiskers = Convert.ToInt32(whiskersInput.Text);
+
+                subtotal = numOflegs * spiderlegs * numOfslime * snailslime * numOfwhiskers * ratwhiskers;
+                taxamount = subtotal * taxrate;
+                totalprice = subtotal * taxamount;
+
+                sublabel.Text = subtotal.ToString("C");
+                taxlabel.Text = taxamount.ToString("C");
+                totallabel.Text = totalprice.ToString("C");
+            }
+            catch
+            {
+                sublabel.Text = "Input Error";
+                taxlabel.Text = "";
+                totallabel.Text = "";
+            }
+        }
+    }
+}
